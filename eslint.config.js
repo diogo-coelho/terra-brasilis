6f397626363871c57f0784c252791d3a2392ea7b
Diogo Coelho <diogooliveiracoelho@gmail.com>
2026-01-05 07:01:21 -0300

    Correção do Eslint para Eslint Flat

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+const js = require('@eslint/js')
+const tsParser = require('@typescript-eslint/parser')
+const tsPlugin = require('@typescript-eslint/eslint-plugin')
+const prettierPlugin = require('eslint-plugin-prettier')
+const globals = require('globals')
+
+module.exports = [
+  {
+    ignores: ['node_modules', 'dist']
+  },
+
+  js.configs.recommended,
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': 'error'
+    }
+  }
+];
